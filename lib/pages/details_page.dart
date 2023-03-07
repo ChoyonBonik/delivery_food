5760c51d5b7234301953e72dde5c3f9c68bad10a
Choyon Bonik <choyonbonik@gmail.com>
2023-03-07 16:57:30 +0600

    details page

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DetailsPage extends StatefulWidget {
   const DetailsPage({Key? key}) : super(key: key);
@@ -10,6 +11,65 @@ class DetailsPage extends StatefulWidget {
 class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _appBar(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomLeft,
+              colors: [
+            Color(0xffFE8C00),
+            Colors.yellow.shade200
+          ])
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 15,
+          ),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Stack(
+              children: [
+                Column(
+                  children: [
+                    Image(image: AssetImage('images/fi.png'))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  PreferredSize _appBar() {
+    return PreferredSize(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.yellow.shade200,
+          ),
+          child: Padding(
+            padding: EdgeInsets.only(
+              right: 25,
+            ),
+            child: Container(
+              child: SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.keyboard_arrow_left, size: 35,)),
+                    CircleAvatar(
+                      backgroundColor: Colors.deepOrange.shade500,
+                      child: Icon(FontAwesomeIcons.heart, color: Colors.white, size: 20),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        preferredSize: Size.fromHeight(150));
   }
 }
