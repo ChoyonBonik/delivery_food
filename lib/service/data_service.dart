02b7f5d9621802fc164da6418dc82ac982cd696c
Choyon Bonik <choyonbonik@gmail.com>
2023-03-07 14:30:39 +0600

    initial commit

---
+++ lib/service/data_service.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DateService extends StatefulWidget {
+  const DateService({Key? key}) : super(key: key);
+
+  @override
+  State<DateService> createState() => _DateServiceState();
+}
+
+class _DateServiceState extends State<DateService> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
